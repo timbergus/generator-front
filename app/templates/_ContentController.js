acdd445662f8d157a7b3f7f1be8567495c910523
timbergus <timbergus@gmail.com>
2014-05-09 14:58:01 +0200

    Files name changed.

---
+++ app/templates/_ContentController.js
@@ -0,0 +1,17 @@
+/*globals app */
+
+app.controller('ContentController', ['$scope', function ($scope) {
+    $scope.content = {
+        title       : '<%= appName %>',
+        description : '<%= appDescription %>',
+        version     : '<%= appVersion %>'
+    };
+
+    $scope.alerts = [
+    	{ type: 'success', msg: 'Wellcome to your first and amazing web application!' }
+  	];
+
+  	$scope.closeAlert = function(index) {
+    	$scope.alerts.splice(index, 1);
+  	};
+}]);
