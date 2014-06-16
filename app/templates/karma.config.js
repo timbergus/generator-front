2c57d2bf67eafbf6e5412c0c99b82f10c948acfc
timbergus <timbergus@gmail.com>
2014-06-16 16:05:57 +0200

    Calendar UI added to the karma configuration file.

--- app/templates/karma.config.js
+++ app/templates/karma.config.js
@@ -19,6 +19,7 @@ module.exports = function(config) {
             'bower_components/angular-animate/angular-animate.js',
             'bower_components/angular-bootstrap/ui-bootstrap.js',
             'bower_components/angular-bootstrap/ui-bootstrap-tpls.js',
+            'bower_components/angular-ui-calendar/src/calendar.js',
             'bower_components/angular-translate/angular-translate.js',
             'bower_components/angular-translate-loader-static-files/angular-translate-loader-static-files.js',
             'bower_components/angular-sanitize/angular-sanitize.js',
